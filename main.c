e3c4cfcef9094fcbc6aecffb6a463288bc0493f9
onokatio <onokatio@gmail.com>
2019-06-27 10:11:29 +0900

    draw

--- main.c
+++ main.c
@@ -13,22 +13,69 @@ struct Player{
 	int y;
 };
 
+
+void draw(){
+	int i;
+	int j;
+	int vram[fieldX][fieldY];
+
+	for(i=0;i<fieldX;i++){
+		for(j=0;j<fieldY;j++){
+			vram[i][j] = ' ';
+		}
+	}
+
+	printf("+");
+	for(i=0;i<fieldX;i++){
+		printf("-");
+	}
+	printf("+\n");
+	for(j=0;j<fieldY;j++){
+		printf("|");
+		for(i=0;i<fieldX;i++){
+			printf("%c",vram[i][j]);
+		}
+		printf("|\n");
+	}
+	printf("+");
+	for(i=0;i<fieldX;i++){
+		printf("-");
+	}
+	printf("+\n");
+}
+
+void input(){
+
+}
+
+void calc(){
+}
+
+void play(){
+	while(1){
+		draw();
+		input();
+		calc();
+	}
+}
+
+void init(int array[fieldX][fieldY]){
+	int i;
+	int j;
+
+	for(i=0;i<fieldX;i++){
+		for(j=0;j<fieldY;j++){
+			array[i][j] = 0;
+		}
+	}
+	
+}
+
 int main(void){
 
 	//int robot[fieldX][fieldY];
 	struct Robot robot[40];
 	struct Player player;
 
-	init(robot);
-	init(player);
-	
 	play();
-}
-
-void play(){
-	while(true){
-		draw();
-		input();
-		calc();
-	}
 }
